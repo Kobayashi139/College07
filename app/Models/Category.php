634e3812ba78f706e2aeab94939f1d6b9693bfc9
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-09-23 17:46:44 +0900

    9-2 error_nashi

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,11 +9,17 @@ class Category extends Model
 {
     use HasFactory;
     
+    public function getByCategory(int $limit_count = 5)
+    {
+        return $this->posts()->with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
+
+    }
+    
     public function posts()   
     {
-         return $this->hasMany(Post::class);  
-    }     
-     
+        return $this->hasMany(Post::class);  
+    }
+         
 }
 
  
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,9 +17,9 @@ class Post extends Model
         'category_id'
         ];
     
-    public function getPaginateByLimit(int $limit_count = 10)
+    public function getPaginateByLimit(int $limit_count = 5)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this::with('category')->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
     
     public function category()
