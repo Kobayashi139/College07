7fdb54afd426bd7d289f9780f49dc7005c61d6ba
Kobayashi139 <kobayashi.shi.00@gmail.com>
2023-09-26 10:36:06 +0900

    Laravel login dekinai

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,27 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Posts</title>
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    </head>
-    <body>
-          <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>   
-            </div>
-            <a href="">{{ $post->category->name }}</a>
-        </div> 
-        <div class="edit">
-          <a href="/posts/{{ $post->id }}/edit">edit</a>
-        </div>
-          <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+   <x-app-layout>
+      <x-slot name="heder">
+        <head>
+            <meta charset="utf-8">
+            <title>Posts</title>
+            <!-- Fonts -->
+            <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        </head>
+      </x-slot>
+      <body>
+            <h1 class="title">
+              {{ $post->title }}
+          </h1>
+          <div class="content">
+              <div class="content__post">
+                  <h3>本文</h3>
+                  <p>{{ $post->body }}</p>   
+              </div>
+              <a href="">{{ $post->category->name }}</a>
+          </div> 
+          <div class="edit">
+            <a href="/posts/{{ $post->id }}/edit">edit</a>
+          </div>
+            <div class="footer">
+              <a href="/">戻る</a>
+          </div>
+      </body>
+    </x-app-layout>
 </html>
